49574f847e9d82f63411429805f8dad75c4f3c41
ruthuswamy <ruthu.gowda5@gmail.com>
2015-06-01 18:29:07 +0530

    Initial Commit

---
+++ Camerafileupload1/src/com/example/camerafileupload1/Config.java
@@ -0,0 +1,10 @@
+package com.example.camerafileupload1;
+
+public class Config {
+	 // File upload url (replace the ip with your server address)
+    public static final String FILE_UPLOAD_URL = "http://192.168.1.3:8383/AndroidFileUpload/fileUpload.php";
+     
+    // Directory name to store captured images and videos
+    public static final String IMAGE_DIRECTORY_NAME = "Android File Upload";
+
+}
